2e5081be27afa09498a413b05f23e15d1341a94e
gilbert-ku <gilbert45ku@gmail.com>
2023-07-07 16:08:27 +0300

    add event listener to login forn

--- script.js
+++ script.js
@@ -5,7 +5,21 @@ const password = document.getElementById("password");
 const login = document.getElementById("login");
 const form = document.querySelector("form");
 
-
+// add event listner to log in page
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const emailValue = email.value;
+    const passwordValue = password.value;
+  // check password 
+    if (emailValue === "example@example.com" && passwordValue === "password") {
+        alert("Login successful!");
+        // Redirect to another page
+        window.location.href = "start.html";
+    } else {
+        alert("Invalid email or password. Please try again.");
+    }
+});
+ 
 
 
 
