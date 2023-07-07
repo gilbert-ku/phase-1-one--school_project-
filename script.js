6dc894c600d5a08b5acb549b8390a0e804c31642
gilbert-ku <gilbert45ku@gmail.com>
2023-07-07 16:06:50 +0300

    get element in js

---
+++ script.js
@@ -0,0 +1,54 @@
+//https://studentprofile.onrender.com/student
+// get elements
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const login = document.getElementById("login");
+const form = document.querySelector("form");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
