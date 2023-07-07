fe164e46f4c8e33ffc9a18910cfdcdf91fc4dd5a
gilbert-ku <gilbert45ku@gmail.com>
2023-07-07 17:43:29 +0300

    add student first name

--- data-base.js
+++ data-base.js
@@ -2,7 +2,18 @@ fetch("https://studentprofile.onrender.com/student")
 .then(response => response.json())
 .then(data => {
  console.log(data)
+    const main = document.getElementById("school-data")
+    const listName = document.getElementById("student-name")
 
+    // iterate 
+    data.forEach((student) => {
+        let studentNames = document.createElement("li")
+        studentNames.textContent = student.first_name
+        listName.appendChild(studentNames)
+
+        const studentProfile = document.createElement("div");
+        
+    }) 
 })
 .catch(error => {
     console.log("Error:", error);
