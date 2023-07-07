eb919390893637295496ed15cde7d091f8650883
gilbert-ku <gilbert45ku@gmail.com>
2023-07-07 18:01:52 +0300

    add student profile and performance

--- data-base.js
+++ data-base.js
@@ -12,7 +12,19 @@ fetch("https://studentprofile.onrender.com/student")
         listName.appendChild(studentNames)
 
         const studentProfile = document.createElement("div");
+        studentProfile.innerHTML = `
         
+        <h1>Student Profile </h1>
+        <p>First name; ${student.first_name}</p>
+        <p>LAst name; ${student.last_name}</p>
+        <p>Gender; ${student.gender}</p>
+        <p>Admission Number; ${student.admin_No}</p>
+        <p>Class; ${student.class}</p>
+        <p>Last Exam; ${student.End_term_Exam}</p>
+
+        `
+        main.appendChild(studentProfile)
+
     }) 
 })
 .catch(error => {
